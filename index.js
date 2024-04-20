d582554efca4145ac32215eb943f57c3457440f5
Abdul Basit Khalsai <abdulbasitkhalsai@gmail.com>
2024-04-20 12:07:53 +0500

    Calculator code updated.

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+//Prompt number & operator
+const answer = await inquirer.prompt([
+    {
+        message: "Please enter first number",
+        name: "firstNumber",
+        type: "number",
+    },
+    {
+        message: "Please enter second number",
+        name: "secondNumber",
+        type: "number"
+    },
+    {
+        message: "Please select operator to perform Operation",
+        name: "operator",
+        type: "list",
+        choices: ["+", "-", "*", "/"]
+    }
+]);
+if (answer.operator === "+") {
+    console.log(`Your answer is ${answer.firstNumber + answer.secondNumber}`);
+}
+else if (answer.operator === "-") {
+    console.log(`Your answer is ${answer.firstNumber - answer.secondNumber}`);
+}
+else if (answer.operator === "*") {
+    console.log(`Your answer is ${answer.firstNumber * answer.secondNumber}`);
+}
+else if (answer.operator === "/") {
+    console.log(`Your answer is ${answer.firstNumber / answer.secondNumber}`);
+}
+c;
